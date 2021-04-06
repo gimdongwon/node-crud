2cf0d99c780432372cab9fa10ef3b80a05b79c2d
Dongwon 🙉 <dongwonkim@Dongwonui-MacBookPro.local>
2021-04-07 02:09:40 +0900

    add nodemon

--- index.js
+++ index.js
@@ -5,9 +5,31 @@ var initialData = fs.readFileSync('initialDB.json');
 var movieList = JSON.parse(initialData);
 
 var app = express()
+const port = process.env.PORT || 3000;
+
+app.get("movies", showMovieList);
 
 app.get("/", (req, res)=>{
     res.send("Hello Node.js")
 })
 
-app.listen(3000);
+function showMovieList(req, res){
+    var data = [];
+    movieList.forEach(movie => {
+        var info ={
+            movieId: movie.movieId,
+            title: movie.title
+        };
+        data.push(info)
+    });
+    var result = {
+        count: data.length,
+        data,
+    }
+    res.send(result)
+    console.log("asdf")
+}
+
+app.listen(3000, ()=>{
+    console.log(`server is listening at localhost:${port}`)
+});
