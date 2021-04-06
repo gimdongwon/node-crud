3a14bf26c1496d1d8e83ce101c482e46d92ff1da
Dongwon 🙉 <dongwonkim@Dongwonui-MacBookPro.local>
2021-04-07 03:24:13 +0900

    add ejs

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ var app = express()
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(require("./movieRouter"))
-app.use(handleError);
 
 app.get("/", (req, res)=>{
     res.send("Hello Node.js")
 })
+
+app.use(handleError);
 
 function handleError(err, req, res, next){
     console.log('Error: ', err);
